fda6657f53727685c0556f0069f5461eec296987
Justin Sitter <justin@jaytria.com>
2019-08-20 22:49:48 -0700

    Update format

--- src/utils/check_project.php
+++ src/utils/check_project.php
@@ -8,7 +8,8 @@ foreach($_GET as $key => $url){
   $fp = fsockopen($url, 80);
   fputs($fp, "GET / HTTP/1.1\r\n");
   fputs($fp, "Host: {$url}\r\n\r\n");
-  $http_response .= fgets($fp, 128);
+  $http_response .= fgets($fp, 1128);
+  // Don't bother waiting til the end of the file
   fclose($fp);
   array_push($xml, $url);
   // echo nl2br(htmlentities($http_response, ENT_QUOTES, 'UTF-8'));
